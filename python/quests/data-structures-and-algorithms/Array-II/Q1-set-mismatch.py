5b1f0316d9fc129120ba6ade67379871e450ec9c
ThisEVAN1 <evanxu365@gmail.com>
2025-11-19 21:04:46 -0800

    Q1-set-mismatch.py

---
+++ python/quests/data-structures-and-algorithms/Array-II/Q1-set-mismatch.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findErrorNums(self, nums):
+        duplicate = None
+        missing = None
+        
+        for i in range(1, len(nums) + 1):
+            amount = nums.count(i)
+
+            if amount == 2:
+                duplicate = i
+            elif amount == 0:
+                missing = i
+        
+        return [duplicate, missing]
